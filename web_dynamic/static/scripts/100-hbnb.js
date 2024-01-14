de60fe4c5dd7209e65a0b300c7e5df9279215a78
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-01-15 02:57:32 +0300

    Task 6

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,31 +1,50 @@
 #!/usr/bin/node
 
 $(document).ready(function () {
-  const $checkboxes = $('input[type="checkbox"]');
-  const $amenities = $('div.amenities h4');
-  const $apiStatus = $('div#api_status');
-  const $places = $('section.places');
+  const amenitiesHeader = $('.amenities h4');
+  const locationsHeader = $('.locations h4');
+  const placesSection = $('section.places');
 
-  let checked = [];
+  let ls_amen = {};
+  let ls_cities = {};
+  let ls_states = {};
 
-  $checkboxes.change(function () {
+  $('. amenities input[type="checkbox"]').click(function () {
     if (this.checked) {
-      checked.push($(this).attr('data-id'));
+      ls_amen[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
-      checked.splice(checked.indexOf($(this).attr('data-id')), 1);
+      delete ls_amen[$(this).attr('data-id')];
     }
-    if (checked.length > 0) {
-      $amenities.text(checked.join(', '));
-    } else {
-      $amenities.html('&nbsp;');
-    }
+    amenitiesHeader.text(Object.values(ls_amen).join(', '));
   });
 
-  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
-    if (data.status === 'OK') {
-      $apiStatus.addClass('available');
+  $('.locations ul.popover li h2 input[type="checkbox"]').click(function () {
+    if (this.checked) {
+      ls_states[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
-      $apiStatus.removeClass('available');
+      delete ls_states[$(this).attr('data-id')];
+    }
+    locationsHeader.text(Object.values(ls_states).concat(Object.values(ls_cities)).join(', '));
+  });
+
+  $('.locations ul.popover li ul li input[type="checkbox"]').click(function () {
+    if (this.checked) {
+      ls_cities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete ls_cities[$(this).attr('data-id')];
+    }
+    locationsHeader.text(Object.values(ls_states).concat(Object.values(ls_cities)).join(', '));
+  });
+
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    type: 'GET',
+    success: function (data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
     }
   });
 
@@ -51,7 +70,7 @@ $(document).ready(function () {
                           ${place.description}
                         </div>
                       </article>`;
-        $places.append(html);
+        placesSection.append(html);
       }
     }
   });
@@ -62,9 +81,9 @@ $(document).ready(function () {
       type: 'POST',
       contentType: 'application/json',
       dataType: 'json',
-      data: JSON.stringify({ amenities: checked }),
+      data: JSON.stringify({ amenities: Object.keys(ls_amen), states: Object.keys(ls_states), cities: Object.keys(ls_cities) }),
       success: function (data) {
-        $places.empty();
+        placesSection.empty();
         for (const place of data) {
           const html = `<article>
                           <div class="title_box">
@@ -80,7 +99,7 @@ $(document).ready(function () {
                             ${place.description}
                           </div>
                         </article>`;
-          $places.append(html);
+          placesSection.append(html);
         }
       }
     });
